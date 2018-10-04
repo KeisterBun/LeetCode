bafdbab0281c513f6b58f304e682229a1f900c5f
Keith <keith.drew.13@gmail.com>
2018-10-04 17:52:12 -0400

    messing around

--- Golang/AddTwoNumbers/add_two_numbers.go
+++ Golang/AddTwoNumbers/add_two_numbers.go
@@ -11,52 +11,51 @@ type ListNode struct {
 }
 
 func main() {
+	head1 := addNode(nil, createNode(3))
+	head1 = addNode(head1, createNode(4))
+	head1 = addNode(head1, createNode(2))
 
-     head1 := addNode(nil, createNode(3))
-     head1 = addNode(head1, createNode(4))
-     head1 = addNode(head1, createNode(2))
-     
-     printList(head1)
+	printList(head1)
 
-     head2 := addNode(nil, createNode(4))
-     head2 = addNode(head2, createNode(6))
-     head2 = addNode(head2, createNode(5))
+	head2 := addNode(nil, createNode(4))
+	head2 = addNode(head2, createNode(6))
+	head2 = addNode(head2, createNode(5))
 
-     printList(head2)
+	printList(head2)
 }
 
 func addNode(head *ListNode, newNode *ListNode) *ListNode {
-     if head == nil {
-     	head = newNode
-	newNode.Next = nil
-     } else {
-       	newNode.Next = head
-	head = newNode
-     }
+	if head == nil {
+		head = newNode
+		newNode.Next = nil
+	} else {
+		newNode.Next = head
+		head = newNode
+	}
 
-     return head
+	return head
 }
 
 func printList(head *ListNode) {
-     if head == nil {
-     	return
-     }
+	if head == nil {
+		return
+	}
 
-     fmt.Println(head.Val)
-     printList(head.Next)
+	fmt.Println(head.Val)
+	printList(head.Next)
 }
 
 func createNode(value int) *ListNode {
-     returnValue := &ListNode{value, nil}
-     return returnValue
+	returnValue := &ListNode{value, nil}
+	return returnValue
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-     if l1 == nil || l2 == nil {
-     	    return nil
+	if l1 == nil || l2 == nil {
+		return nil
 	}
 
 	// TODO: implement the solution :D
-	
+
 	return nil
 }
