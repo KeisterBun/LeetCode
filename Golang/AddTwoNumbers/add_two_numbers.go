ed20813b555aab74089c8597288ac7e90951a11f
Keith <keith.drew.13@gmail.com>
2018-10-04 18:39:40 -0400

    A little progress

--- Golang/AddTwoNumbers/add_two_numbers.go
+++ Golang/AddTwoNumbers/add_two_numbers.go
@@ -1,10 +1,13 @@
-/* Written by Keith Drew, 2018
-* This is a solution to LeetCode Problem #2: "Add Two Numbers," written in Go
- */
+/*
+Package main takes two numbers represented by linked lists and adds them together, as per LeetCode.com problem #2.
+
+Written by Keith Drew, 2018.
+*/
 package main
 
 import "fmt"
 
+// ListNode : The struct used for linked lists.
 type ListNode struct {
 	Val  int
 	Next *ListNode
@@ -22,6 +25,10 @@ func main() {
 	head2 = addNode(head2, createNode(5))
 
 	printList(head2)
+
+	final := addTwoNumbers(head1, head2)
+
+	printList(final)
 }
 
 func addNode(head *ListNode, newNode *ListNode) *ListNode {
@@ -55,7 +62,14 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 		return nil
 	}
 
-	// TODO: implement the solution :D
+	// TODO: Hmm. Getting the lists in reverse means we can easily handle the carry, but what's the right recursion here?
+	// Maybe recursion isn't the best answer?
+	if l1.Val+l2.Val >= 10 {
+		newNode := createNode((l1.Val + l2.Val) % 10)
+
+	} else {
+		newNode := createNode(l1.Val + l2.Val)
+	}
 
 	return nil
 }
