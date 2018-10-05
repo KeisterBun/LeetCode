1b63186a3497955b7b354710676d9ea2ae05d4fb
Keith <keith.drew.13@gmail.com>
2018-10-05 16:35:21 -0400

    Added starting file for Add Two Numbers problem from LeetCode in C++

---
+++ CPP/AddTwoNumbers/add_two_numbers.cpp
@@ -0,0 +1,28 @@
+/*
+Class Solution takes two numbers represented by linked lists and adds them together, as per LeetCode.com problem #2.
+
+Written by Keith Drew, 2018.
+*/
+#include <iostream>
+#include <string>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+    public:
+        ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+            return NULL;
+        }
+};
+
+int main() {
+    Solution solution;
+
+    std::cout << "Output" << std::endl;
+
+    return 0;
+}
